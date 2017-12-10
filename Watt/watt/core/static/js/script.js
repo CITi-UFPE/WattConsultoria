dabf997f24bb8bbc7f6bde8bd6e08f57c4007c09
Victor Aguiar <vag2@cin.ufpe.br>
2017-12-10 16:08:10 +0000

    Criação do carrossel de depoimentos

--- Watt/watt/core/static/js/script.js
+++ Watt/watt/core/static/js/script.js
@@ -1,5 +1,7 @@
+/*                  1º CARROSSEL              */
 var slideIndex = 1;
 showSlides(slideIndex);
+window.location = "#pg1";
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -23,4 +25,32 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+
+/*                  CARROSSEL DEPOIMENTOS              */
+var slideIndexDepoimentos = 1;
+showSlidesDepoimentos(slideIndexDepoimentos);
+
+function plusSlidesDepoimentos(n) {
+  showSlidesDepoimentos(slideIndexDepoimentos += n);
+}
+
+function currentSlideDepoimentos(n) {
+  showSlidesDepoimentos(slideIndexDepoimentos = n);
+}
+
+function showSlidesDepoimentos(n) {
+  var i;
+  var slidesDepoimentos = document.getElementsByClassName("slidesDepoimentos");
+  var dotsDepoimentos = document.getElementsByClassName("dotDepoimentos");
+  if (n > slidesDepoimentos.length) {slideIndexDepoimentos = 1} 
+  if (n < 1) {slideIndexDepoimentos = slidesDepoimentos.length}
+  for (i = 0; i < slidesDepoimentos.length; i++) {
+      slidesDepoimentos[i].style.display = "none"; 
+  }
+  for (i = 0; i < dotsDepoimentos.length; i++) {
+      dotsDepoimentos[i].className = dotsDepoimentos[i].className.replace(" activeDepoimentos", "");
+  }
+  slidesDepoimentos[slideIndexDepoimentos-1].style.display = "block"; 
+  dotsDepoimentos[slideIndexDepoimentos-1].className += " activeDepoimentos";
 }
